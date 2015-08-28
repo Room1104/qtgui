f5e462f554f77d124bc2ceeb531aa6b6b1163b73
Jim Finnis <jim.finnis@gmail.com>
2015-08-28 18:39:46 +0100

    speaks with mary

--- scripts/testservice.py
+++ scripts/testservice.py
@@ -2,17 +2,26 @@
 
 import sys
 import rospy
+import actionlib
 import std_srvs.srv
-
+import mary_tts.msg
 
 def pushbutton_callback(req):
+    c = actionlib.SimpleActionClient('speak',mary_tts.msg.maryttsAction)
+    c.wait_for_server()
+    goal = mary_tts.msg.maryttsGoal("wibble")
+    c.send_goal(goal)
+    c.wait_for_result()
+    
     return std_srvs.srv.TriggerResponse(True,"boo");
 
 def add_pushbutton_service():
-    rospy.init_node('pushbutton')
-    s = rospy.Service('pushbutton/one',std_srvs.srv.Trigger,pushbutton_callback)
+    s = rospy.Service('pushbutton/one',std_srvs.srv.Trigger,
+        pushbutton_callback)
     print "pushbutton service up"
     rospy.spin()
     
 if __name__ == "__main__":
+    rospy.init_node('pushbutton')
+    pushbutton_callback("foo")
     add_pushbutton_service()
