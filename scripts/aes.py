746b10db436d93968f6f25aca70f06de50856df6
Jim Finnis <jim.finnis@gmail.com>
2015-08-30 14:59:01 +0100

    People now affect the AES

--- scripts/aes.py
+++ scripts/aes.py
@@ -8,6 +8,7 @@ import std_srvs.srv
 import mary_tts.msg
 from std_msgs.msg import Float64
 from std_msgs.msg import Int8
+from bayes_people_tracker.msg import PeopleTracker
 from math import exp
 
 hconc=0
@@ -23,10 +24,13 @@ releasefactor=rospy.get_param('/aes/release',0.02)
 tempreleasefactor=rospy.get_param('/aes/nudge',1)
 # amount released by smiles
 smilereleasefactor=rospy.get_param('/aes/smile',0.2)
+# amount released by people
+peoplereleasefactor=rospy.get_param('/aes/people',0.2)
 
 releaserate=0
 smilecount=0
 tempreleaserate=0
+peoplerelease=0
 
 def sigmoid(x):
     x=(x-centre)/width
@@ -63,8 +67,13 @@ def speakserv(req):
     return std_srvs.srv.TriggerResponse(True,utterance());
 
 def update():
-    global hconc,hlevel,decayrate,releaserate,tempreleaserate
-    r = releaserate+tempreleaserate*tempreleasefactor+smilecount*smilereleasefactor
+    global hconc,hlevel,decayrate,releaserate,tempreleaserate,peoplerelease
+    global peoplereleasefactor,smilereleasefactor,tempreleasefactor
+    global smilecount
+    r = releaserate
+    r = r+tempreleaserate*tempreleasefactor
+    r = r+smilecount*smilereleasefactor
+    r = r+peoplerelease*peoplereleasefactor
     r = r*releasefactor*(0.95-hconc)
     tempreleaserate=0
     hconc = (hconc+r)*decayrate
@@ -95,11 +104,50 @@ def bumprelease(r):
     tempreleaserate = r
     return std_srvs.srv.TriggerResponse(True,'');
 
+# fuzzification
+def ramp(x,a,b):
+    if x<a:
+        return 0
+    elif x>b:
+        return 1
+    else:
+        return (x-a)/(b-a)
+def peak(x,v,w):
+    return min(ramp(x,v-w,v),1.0-ramp(x,v,v+w))
+
+def setpeople(p):
+    global peoplerelease
+    t=0
+    good=0
+    bad=0
+    closeppl=0
+    # fuzzy logic on each person
+    for d,a in zip(p['distances'],p['angles']):
+        # close is up to 4m away
+        close = 1.0-ramp(d,1.0,4.0)
+        closeppl = closeppl+close
+        # very close
+        veryclose = 1.0-ramp(d,0.0,2.0)
+        # behind
+        behind = peak(a,3.1415927,0.5)
+        # "good" is close and not very close and not behind
+        good = max(good,min(close,1.0-veryclose,1.0-behind))
+        # bad is (close and behind) or veryclose
+        bad = max(bad,min(close,behind),veryclose)
+    # we end up with "good/bad", but lots of close people is very bad.
+    bad = max(bad,ramp(close,2,4))
+    peoplerelease = good-bad
+    sys.stderr.write(str(peoplerelease)+"\n")
+        
+
 def startsubscribers():
-    subsmile = rospy.Subscriber('smile_detector',Int8,
+    rospy.Subscriber('smile_detector',Int8,
         lambda x: setsmile(x.data))
     rospy.Subscriber('hrelease',Float64,
         lambda x: setrelease(x.data))
+    rospy.Subscriber('/people_tracker/positions',PeopleTracker,
+        lambda x: setpeople(x.data))
+    
         
 def startservices():
     rospy.Service('aes/nice',std_srvs.srv.Trigger,
@@ -110,6 +158,9 @@ def startservices():
 
 def start():
     update()
+    p={'distances':[3,5,4],'angles':[0,3,0]}
+    setpeople(p)
+
     rospy.init_node("aes",anonymous=True)
     startsubscribers()
     startservices()
