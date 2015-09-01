9a631dcbf3d801105f708ca9ed761d6679e52de6
Jim Finnis <jim.finnis@gmail.com>
2015-09-01 13:46:36 +0100

    home hormone release slightly higher

--- scripts/aes.py
+++ scripts/aes.py
@@ -38,7 +38,7 @@ smilereleasefactor=rospy.get_param('/aes/smile',0.4)
 # amount released by people
 peoplereleasefactor=rospy.get_param('/aes/people',0.1)
 # amount released by home node
-homereleasefactor=rospy.get_param('/aes/home',0.1)
+homereleasefactor=rospy.get_param('/aes/home',0.15)
 
 
 releaserate=0
